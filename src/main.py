cfabe574c44fca290f87139d19c6619b94aee158
Roman Ivanov <sitdoff@gmail.com>
2025-07-05 18:54:15 +0300

    update main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,28 @@
-import uvicorn
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
+from src.api import router as api_router
 from src.config import settings
+from src.database import db_helper
 
-app = FastAPI(prefix=settings.app_settings.api_prefix)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """
+    Функция содержит в себе действия, которые должны быть
+    выполнены до старта приложения и до завершения его работы.
+    """
+    # startup
+    yield
+    # shutdown
+    print("Disposing database connection...")
+    await db_helper.dispose()
+    print("Connection disposed")
+
+
+app = FastAPI(
+    prefix=settings.api.api_prefix,
+    lifespan=lifespan,
+)
+app.include_router(api_router)
