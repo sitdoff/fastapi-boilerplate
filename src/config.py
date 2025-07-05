e1379391f36602dae7f7014a395825a185cfb553
Roman Ivanov <sitdoff@gmail.com>
2025-07-05 18:54:24 +0300

    add config.py

---
+++ src/config.py
@@ -0,0 +1,63 @@
+from pydantic import BaseModel, PostgresDsn
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class ApiSettings(BaseModel):
+    """
+    Настройки API.
+    """
+
+    api_prefix: str = "from .env"
+
+
+class DatabaseSettings(BaseModel):
+    """
+    Настройки для взаимодействия с базой данных.
+    """
+
+    host: str = "from .env"
+    port: str = "from .env"
+    name: str = "from .env"
+    user: str = "from .env"
+    password: str = "from .env"
+    echo: bool = True
+    echo_pool: bool = True
+    pool_size: int = 5  # Количество постоянно открытых соединений
+    max_overflow: int = 10  # Количество дополниельных соединений сверх pool_size
+    convention: dict[str, str] = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_N_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
+
+    @property
+    def url(self) -> str:
+        """
+        Создаёт url для подключения к базе данных.
+        """
+        return str(
+            PostgresDsn(
+                f"postgresql+asyncpg://{self.user}:{self.password}@{self.host}:{self.port}/{self.name}"
+            ),
+        )
+
+
+class Settings(BaseSettings):
+    """
+    Объедененные настройки.
+    """
+
+    api: ApiSettings = ApiSettings()
+    db: DatabaseSettings = DatabaseSettings()
+
+    model_config = SettingsConfigDict(
+        env_file=(".env.example", ".env"),
+        case_sensitive=False,
+        env_prefix="APP__",
+        env_nested_delimiter="__",
+    )
+
+
+settings = Settings()
